7ffe4f594d97771a9b7bfb7de904514593637c07
Eva Barrett <Evabarrett23@Gmail.com>
2018-06-05 15:00:04 -0700

    added score board and oscar

--- codetest.js
+++ codetest.js
@@ -33,3 +33,81 @@ $(window).keydown(moveMonster);
 
 
 // test things out
+
+
+
+$('.cookie1').click(function() {
+  $('.cookie1').gone();
+});
+
+$('.cookie2').click(function() {
+  $('.cookie2').toggle();
+});
+
+$('.cookie3').click(function() {
+  $('.cookie3').toggle();
+});
+
+$('.cookie4').click(function() {
+  $('.cookie4').toggle();
+});
+
+$('.cookie5').click(function() {
+  $('.cookie5').toggle();
+});
+
+$('.cookie6').click(function() {
+  $('.cookie6').toggle();
+});
+
+$('.cookie7').click(function() {
+  $('.cookie7').toggle();
+});
+
+$('.cookie8').click(function() {
+  $('.cookie8').toggle();
+});
+
+$('.cookie9').click(function() {
+  $('.cookie9').toggle();
+});
+
+function reset
+
+
+var cookieOne = {
+  element: $('#cookie1'),
+  position: 0
+}
+
+  $('.box').click(cardFlip);
+
+
+
+
+var cookieTwo = {
+  element: $('#cookie2'),
+  position: 0
+};
+
+var monster = {
+  element: $('#blue-monster'),
+  position: 0
+};
+
+var moveMonster = function(e) {
+  // does the event key property equal left?
+  if (e.key === 'ArrowLeft') {
+    e.preventDefault();
+    monster.position -= 15;
+    monster.element.css("left", monster.position + "px");
+    console.log("moveMonster");
+  } else if (e.key === 'ArrowRight') {
+      e.preventDefault();
+      monster.position += 15;
+      monster.element.css("left", monster.position + "px");
+      console.log('moveMonster right')
+  }
+};
+
+$(window).keydown(moveMonster);
--- script.js
+++ script.js
@@ -1,57 +1,74 @@
+var score = 0;
+var cookies = [];
 
 $(document).ready(function(){
-  $(".cookie1").click(function(){
-    $(".cookie1").hide();
+  $.each($('.s'), function(){
+
+    // .cookie1 {
+    //   position: absolute;
+    //   /* top: 15vh; */
+    //   left: 15vw;
+    //   height: 80px;
+    //   width: 80px;
+    //   background: red;
+    //   animation-delay: 4.2s;
+    //   border-radius: 50%;
+    // }
+  });
+  $(".s").click(function(){
+    $(this).hide();
+    score++
+    console.log(score)
   });
 });
 
-$(document).ready(function(){
-  $(".cookie2").click(function(){
-    $(".cookie2").hide();
-  });
-});
-
-$(document).ready(function(){
-  $(".cookie3").click(function(){
-    $(".cookie3").hide();
-  });
-});
-
-$(document).ready(function(){
-  $(".cookie4").click(function(){
-    $(".cookie4").hide();
-  });
-});
-
-$(document).ready(function(){
-  $(".cookie5").click(function(){
-    $(".cookie5").hide();
-  });
-});
-
-$(document).ready(function(){
-  $(".cookie6").click(function(){
-    $(".cookie6").hide();
-  });
-});
-
-$(document).ready(function(){
-  $(".cookie7").click(function(){
-    $(".cookie7").hide();
-  });
-});
-
-$(document).ready(function(){
-  $(".cookie8").click(function(){
-    $(".cookie8").hide();
-  });
-});
-
-$(document).ready(function(){
-  $(".cookie9").click(function(){
-    $(".cookie9").hide();
-  });
-});
+// $(document).ready(function(){
+//   $(".cookie2").click(function(){
+//     $(".cookie2").hide();
+//   });
+// });
+//
+// $(document).ready(function(){
+//   $(".cookie3").click(function(){
+//     $(".cookie3").hide();
+//   });
+// });
+//
+// $(document).ready(function(){
+//   $(".cookie4").click(function(){
+//     $(".cookie4").hide();
+//   });
+// });
+//
+// $(document).ready(function(){
+//   $(".cookie5").click(function(){
+//     $(".cookie5").hide();
+//   });
+// });
+//
+// $(document).ready(function(){
+//   $(".cookie6").click(function(){
+//     $(".cookie6").hide();
+//   });
+// });
+//
+// $(document).ready(function(){
+//   $(".cookie7").click(function(){
+//     $(".cookie7").hide();
+//   });
+// });
+//
+// $(document).ready(function(){
+//   $(".cookie8").click(function(){
+//     $(".cookie8").hide();
+//   });
+// });
+//
+// $(document).ready(function(){
+//   $(".cookie9").click(function(){
+//     $(".cookie9").hide();
+//   });
+// });
 
     var monster = {
       element: $('#blue-monster'),
