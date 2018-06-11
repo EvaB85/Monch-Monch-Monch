bb3b99a80e4166433917f989dc9b633cb62145b7
Eva Barrett <Evabarrett23@Gmail.com>
2018-06-10 19:43:51 -0700

    fixed positioning and z-index issues

--- script.js
+++ script.js
@@ -4,12 +4,10 @@ var fallingImages = $('.falling');
 var remainingTime = 30;
 var fallSpeed = 30;
 var playerPoints = null;
-// var cookieImage = src="images/cookie-monster-empty.png"
 
 $('#player-one').click(function() {
   score = 0;
   playerPoints = $('.points1');
-
 });
 
 $('#player-two').click(function() {
@@ -17,20 +15,26 @@ $('#player-two').click(function() {
   playerPoints = $('.points2');
 });
 
-
-// get document ready
 // run function for each cookie
 // recieve one point per user's click
-$(document).ready(function(){
-  $('.falling').hide();
-  $.each($('.falling'), function(){
+
+$('.falling').hide();
+$.each($('.falling'), function(){
+
 });
-  $('.falling').click(function(){
-    $(this).hide();
-      score++;
-    playerPoints.text(score);
-    });
+
+$('.falling.cookie').click(function(){
+  $(this).hide();
+    score++;
+  playerPoints.text(score);
 });
+
+$('.falling.pepper').click(function(){
+  $(this).hide();
+    score--;
+  playerPoints.text(score);
+});
+
 
 var randomNum = function(max) {
   return Math.ceil(Math.random() * max);
@@ -49,16 +53,11 @@ var endGame = function() {
 // function that starts timer
 var startTimer = function () {
   $('.falling').show();
-  // $('.cookie1').css('animation', 'fall ' + randomNum(12) + 's infinite linear');
-  // for (let i = 0; i < fallingImages.length; i++) {
-    // console.log(fallingImages[i]);
-    // fallingImages[i].css('animation', 'fall ' + randomNum(12) + 's infinite linear');
-  // };
-
   // function that loops over every falling image
   fallingImages.each(function() {
     $(this).css('animation', 'fall ' + randomNum(fallSpeed) + 's infinite linear');
   });
+
   var timer = setInterval(function() {
     remainingTime--;
     $('.timer span').text(remainingTime);
@@ -72,27 +71,15 @@ var startTimer = function () {
   }, 1000);
 };
 
-
-
 // function to call startTimer
 var startGame = function () {
   startTimer()
     $('.start-button').css('visibility', 'hidden');
     $('.play-again').css('visibility', 'hidden');
-    // NEEDS TO WORK BROKEN....
-    // $('.image').css('background-image', cookieImage);
   };
 
 // starts game
 $('.start-button').click(startGame);
-
-
-// blue-monster's position set to zero
-    var monster = {
-      element: $('#blue-monster'),
-      position: 0
-    };
-
 
 // reset game function
 var resetGame = function(){
@@ -100,48 +87,27 @@ var resetGame = function(){
   score = 0;
   playerPoints.text(score);
   remainingTime = 30;
-  // $('.s').show();
-  // cookies to start falling
-  console.log('reset Game!');
 };
 
 $('.play-again').click(resetGame);
 
+var monster = {
+  element: $('#blue-monster'),
+  position: 0
+};
 
+var moveMonster = function(e) {
+  // LEFT
+  if (e.key === 'ArrowLeft') {
+    e.preventDefault();
+    monster.position -= 10;
+    monster.element.css('left', monster.position + 'px');
+    // RIGHT
+  } else if (e.key === 'ArrowRight') {
+    e.preventDefault();
+    monster.position += 10;
+    monster.element.css('left', monster.position + 'px');
+  }
+};
 
-
-
-
-// Original game idea, old code may remove, haven't decided yet
-// function that allows user to move blue-monster to move left and right
-    var moveMonster = function(e) {
-      // does the event key property equal left?
-      console.log('move monster function')
-      if (e.key === 'ArrowLeft') {
-        e.preventDefault();
-        monster.position -= 10;
-        monster.element.css('left', monster.position + 'px');
-        console.log('moveMonster');
-      } else if (e.key === 'ArrowRight') {
-        e.preventDefault();
-        monster.position += 10;
-        monster.element.css('left', monster.position + 'px');
-        console.log('moveMonster right')
-      }
-    };
-
-    // $(window).on('keydown', moveMonster);
-    $(window).keydown(moveMonster);
-
-
-    // if time permits, use array to clean up css for each cookie
-        // .cookie1 {
-        //   position: absolute;
-        //   /* top: 15vh; */
-        //   left: 15vw;
-        //   height: 80px;
-        //   width: 80px;
-        //   background: red;
-        //   animation-delay: 4.2s;
-        //   border-radius: 50%;
-        // }
+$(window).keydown(moveMonster);
