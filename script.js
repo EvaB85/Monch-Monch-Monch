aad115cc77f047d32e89a51847aba34370b341f8
Eva Barrett <Evabarrett23@Gmail.com>
2018-06-10 21:23:25 -0700

    cannot click player two once player one is selected and reset time per round

--- script.js
+++ script.js
@@ -1,18 +1,24 @@
 // gloabl variables
 var score = 0;
 var fallingImages = $('.falling');
-var remainingTime = 30;
+var remainingTime = 5;
 var fallSpeed = 30;
 var playerPoints = null;
+var isGameActive = false;
 
 $('#player-one').click(function() {
-  score = 0;
-  playerPoints = $('.points1');
+  if (!isGameActive) {
+    score = 0;
+    playerPoints = $('.points1');
+    playerPoints.text('0');
+  }
 });
 
 $('#player-two').click(function() {
-  score = 0;
-  playerPoints = $('.points2');
+  if (!isGameActive) {
+    score = 0;
+    playerPoints = $('.points2');
+  }
 });
 
 // run function for each cookie
@@ -52,12 +58,6 @@ var endGame = function() {
 
 // function that starts timer
 var startTimer = function () {
-  $('.falling').show();
-  // function that loops over every falling image
-  fallingImages.each(function() {
-    $(this).css('animation', 'fall ' + randomNum(fallSpeed) + 's infinite linear');
-  });
-
   var timer = setInterval(function() {
     remainingTime--;
     $('.timer span').text(remainingTime);
@@ -71,12 +71,34 @@ var startTimer = function () {
   }, 1000);
 };
 
+// var selectBox = function(){
+//   if (playerPoints) {
+//     $('#player-one').click(function() {
+//       $('#player-one').css('color', 'plum');
+//     }
+//   } else {
+//   };
+
+  //
+  // $("button").click(function(){
+  //     $("p").css("color", "red");
+  // });
+
+
 // function to call startTimer
 var startGame = function () {
-  startTimer()
+  if (playerPoints) {
+    isGameActive = true;
+    startTimer()
     $('.start-button').css('visibility', 'hidden');
     $('.play-again').css('visibility', 'hidden');
-  };
+    $('.falling').show();
+    // function that loops over every falling image
+    fallingImages.each(function() {
+      $(this).css('animation', 'fall ' + randomNum(fallSpeed) + 's infinite linear');
+    });
+  }
+};
 
 // starts game
 $('.start-button').click(startGame);
@@ -84,12 +106,41 @@ $('.start-button').click(startGame);
 // reset game function
 var resetGame = function(){
   $('.start-button').css('visibility', 'visible');
+  isGameActive = false;
   score = 0;
-  playerPoints.text(score);
+  // playerPoints.text(score);
+  playerPoints = null;
   remainingTime = 30;
+  // add text to DOM to update time
 };
 
 $('.play-again').click(resetGame);
+
+// var checkEndGame = function () {
+//   if ('#player-one.points1').text === '#player-two.points2').text {
+//     // ADD MODAL PLAYER ONE WINS
+//   } else if () {
+// // ADD MODAL PLAYER TWO WINS
+//   } else () {
+//   // ADD MODAL TIE GAME
+//   }
+// };
+//
+// var checkEndGame = function(){
+//   var b = boxStatus;
+// // check box 1 2 3 checking to see if box status one is not false
+//   if (b.one !== false && b.one === b.two && b.two === b.three){
+//     if (b.one === "X") {
+//       youWinText.textContent = "PLAYER ONE WINS!"
+//       youWinText.style.fontWeight = "bold";
+//       youWinText.style.transform = "scale(1)";
+//       endGame();
+//     } else if (b.one === "O") {
+//       youWinText.textContent = "PLAYER TWO WINS!"
+//       youWinText.style.fontWeight = "bold";
+//       youWinText.style.transform = "scale(1)";
+//       endGame();
+//     };
 
 var monster = {
   element: $('#blue-monster'),
